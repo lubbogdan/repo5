0a5ee62ac85af749741e782800d68671a7696536
bohdan <bogdan.lybinsky@gmail.com>
2022-04-12 14:46:28 +0300

    money added

---
+++ Money/Money.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money
+{
+    public class Money
+    {
+        public int Amount { get; set; }
+    }
+}
